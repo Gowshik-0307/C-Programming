1622dbb1b76765bdadbc8eda0c3e9795d43697d9
Gowshik-0307 <gowshik9597@gmail.com>
2026-02-28 14:30:22 +0530

    Update Day of the week.c

--- Conditional Statement/Day of the week.c
+++ Conditional Statement/Day of the week.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-int main() {
+int main()
+{
     int day;
     scanf("%d",&day);
 
-    switch (day) {
+    switch (day) 
+    {
         case 1:
             printf("Monday\n");
             break;
